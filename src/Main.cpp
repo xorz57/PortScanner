45e43706eaa14c94cebdfb19432e05509d6645b5
George Fotopoulos <xorz57@gmail.com>
2023-11-14 13:45:06 +0200

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,8 +27,6 @@ bool isUDPPortOpen(const std::string &host, unsigned int port) {
         boost::asio::ip::udp::resolver resolver(io_context);
         boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), host, std::to_string(port));
         boost::asio::ip::udp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-        socket.open(boost::asio::ip::udp::v4());
-        socket.connect(*endpoint_iterator);
         return true;
     } catch (const boost::system::system_error &) {
         return false;
