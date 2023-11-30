93cc10577f10ca4cfc2a0bb17d4491ac3a8edfe2
George Fotopoulos <xorz57@gmail.com>
2023-11-30 12:42:42 +0200

    Use async_connect

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,24 +1,8 @@
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
 
-#include <future>
 #include <iostream>
-#include <mutex>
-#include <vector>
-
-bool IsTCPPortOpen(const std::string &host, unsigned int port) {
-    try {
-        boost::asio::io_context io_context;
-        boost::asio::ip::tcp::socket socket(io_context);
-        boost::asio::ip::tcp::resolver resolver(io_context);
-        boost::asio::ip::tcp::resolver::query query(host, std::to_string(port));
-        boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-        boost::asio::connect(socket, endpoint_iterator);
-        return true;
-    } catch (const boost::system::system_error &) {
-        return false;
-    }
-}
+#include <map>
 
 int main(int argc, char *argv[]) {
     namespace po = boost::program_options;
@@ -30,8 +14,8 @@ int main(int argc, char *argv[]) {
         ("help", "display help message")
         ("host", po::value<std::string>()->default_value("127.0.0.1"), "set host")
         ("protocol", po::value<std::string>()->default_value("tcp"), "set protocol (tcp/udp)")
-        ("begin-port", po::value<unsigned int>()->default_value(0), "set begin-port")
-        ("end-port", po::value<unsigned int>()->default_value(65535), "set end-port")
+        ("begin-port", po::value<unsigned short>()->default_value(0), "set begin-port")
+        ("end-port", po::value<unsigned short>()->default_value(65535), "set end-port")
         ("show", po::value<std::string>()->default_value("open"), "display only 'open', 'closed', or 'all' ports")
     ;
     // clang-format on
@@ -64,8 +48,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    const unsigned int beginPort = vm["begin-port"].as<unsigned int>();
-    const unsigned int endPort = vm["end-port"].as<unsigned int>();
+    const unsigned short beginPort = vm["begin-port"].as<unsigned short>();
+    const unsigned short endPort = vm["end-port"].as<unsigned short>();
     if (beginPort > endPort || endPort > 65535) {
         std::cerr << "Error: Invalid port range. begin-port should be less than or equal to end-port, and both should be in the range [0, 65535]." << std::endl;
         return 1;
@@ -77,23 +61,31 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    std::vector<std::future<void>> futures;
-    std::mutex mutex;
+    boost::asio::io_context io_service;
 
-    for (unsigned int port = beginPort; port <= endPort; ++port) {
-        futures.emplace_back(std::async(std::launch::async, [show, protocol, host, port, &mutex]() -> void {
-            if (protocol == "tcp") {
-                bool status = IsTCPPortOpen(host, port);
-                if ((show == "open" && status) || (show == "closed" && !status) || (show == "all")) {
-                    std::lock_guard lock(mutex);
-                    std::cout << "Port " << port << "/" << protocol << " is " << (status ? "open" : "closed") << "." << std::endl;
+    boost::asio::ip::tcp::resolver resolver(io_service);
+    boost::asio::ip::tcp::resolver::query query(host, "0");
+    boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    if (protocol == "tcp") {
+        std::map<unsigned short, std::unique_ptr<boost::asio::ip::tcp::socket>> sockets;
+        for (unsigned short port = beginPort; port < endPort; port++) {
+            auto s = std::make_unique<boost::asio::ip::tcp::socket>(io_service);
+            auto endpoint = boost::asio::ip::tcp::endpoint(endpoint_iterator->endpoint().address(), port);
+            s->async_connect(endpoint, [show, protocol, port](const boost::system::error_code& error) {
+                if (!error) {
+                    if (show != "closed") {
+                        std::cout << "Port " << port << "/" << protocol << " is " << "open" << "." << std::endl;
+                    }
+                } else {
+                    if (show != "open") {
+                        std::cout << "Port " << port << "/" << protocol << " is " << "closed" << "." << std::endl;
+                    }
                 }
-            }
-        }));
-    }
-
-    for (const std::future<void> &future: futures) {
-        future.wait();
+            });
+            sockets.emplace(port, std::move(s));
+        }
+        io_service.run();
     }
 
     return 0;
